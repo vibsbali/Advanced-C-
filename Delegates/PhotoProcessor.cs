b904aaf55a760959d3e07c0db5aa33664ea51e1d
Vaibhav Bali <vibsbali@gmail.com>
2016-08-31 23:13:19 +1000

    Completed Delegates

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        //public delegate void PhotoFilterHandler(Photo photo);
+
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+            
+            //var filters = new PhotoFilters();
+            //filters.ApplyBrightness(photo);
+            //filters.ApplyContrast(photo);
+            //filters.Resize(photo);
+
+            photo.Save();
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+
+            //Notice we did not call it just pointed to it
+            Action<Photo> filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEye;
+            processor.Process("photo.jpg", filterHandler);
+        }
+
+        static void RemoveRedEye(Photo photo)
+        {
+            Console.WriteLine("Removed Red Eye");
+        }
+    }
+}
