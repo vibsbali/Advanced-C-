c0556f243b213bc4cab6cfaf9931ab7a423a8172
Vaibhav Bali <vibsbali@gmail.com>
2016-09-05 07:26:16 +1000

    Nullable types

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime? date = null;
+
+            //using null collase operator
+            DateTime date2 = date ?? DateTime.Today;
+            //using tertiary operator
+            DateTime date3 = date != null ? date.GetValueOrDefault() : DateTime.Today;
+
+        }
+    }
+}
